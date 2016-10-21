2d3715340bff84997e0850a648a1bf0fb431df2a
Sebatl <Seba_tl@hotmail.com>
2016-10-21 17:02:16 -0200

    Java Card Reader

---
+++ src/main/CardReader.java
@@ -0,0 +1,54 @@
+package main;
+
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.smartcardio.ATR;
+import javax.smartcardio.Card;
+import javax.smartcardio.CardException;
+import javax.smartcardio.CardTerminal;
+import javax.smartcardio.TerminalFactory;
+
+
+
+public class CardReader {
+
+    static CardTerminal terminal;
+    
+public static void prepare(){
+     try{
+        TerminalFactory factory = TerminalFactory.getDefault();
+        List<CardTerminal> terminals = factory.terminals().list();
+        System.out.println("Lectores: " + terminals);
+
+            terminal = terminals.get(0);
+          }catch(Exception e){
+              e.printStackTrace();
+          }
+
+}
+
+public static Card read(){
+        try {
+            Card card = terminal.connect("*");
+            System.out.println("Tarjeta: "+card);
+            
+            ATR atr = card.getATR();
+        byte[] ATR = atr.getBytes();
+        byte[] TuttaCarta;
+        System.out.println("ATR: " +ATR);
+        
+         StringBuilder sb = new StringBuilder(ATR.length * 2);
+        for (int i = 0; i < ATR.length; i++) {
+            sb.append(String.format("%02x", ATR[i]));
+        }
+
+        System.out.println(sb.toString());
+            
+        } catch (CardException ex) {
+            Logger.getLogger(CardReader.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+}
+    
+}
