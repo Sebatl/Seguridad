a1f3cf3bbaa3c723df536cbfb57dfaf5afeec56d
Sebatl <Seba_tl@hotmail.com>
2016-10-21 17:03:33 -0200

    Java Card Reader

--- src/main/DataBaseConnection.java
+++ src/main/DataBaseConnection.java
@@ -26,7 +26,7 @@ public class DataBaseConnection {
 	Connection connection = null;
 	try {
             connection = DriverManager
-            .getConnection("jdbc:mysql://localhost:3306/seguridad","root", "lintux_tl1");
+            .getConnection("jdbc:mysql://localhost:3306/seguridad","root", "root");
 
 	} catch (SQLException e) {
             System.out.println("Connection Failed! Check output console");
