ec06a5170399905e4ec15caefd3740dff777df29
Sebatl <Seba_tl@hotmail.com>
2016-11-04 19:30:29 -0200

    JavaFX disable

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,83 +6,18 @@
 package main;
 
 import GUI.ID_Asker;
-import java.awt.BorderLayout;
-import java.awt.Dimension;
 
-import javax.swing.JApplet;
-import javax.swing.SwingUtilities;
-import javax.swing.UIManager;
-import javax.swing.UnsupportedLookAndFeelException;
+/**
+ *
+ * @author Seba
+ */
+public class Main {
 
-public class Main extends JApplet {
+    public static void main(String args[]) {
+        CardReader.prepare();
 
-    private static final int JFXPANEL_WIDTH_INT = 300;
-    private static final int JFXPANEL_HEIGHT_INT = 250;
-    //private static JFXPanel fxContainer;
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
-
-            @Override
-            public void run() {
-                try {
-                    UIManager.setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
-                } catch (ClassNotFoundException | IllegalAccessException | InstantiationException | UnsupportedLookAndFeelException e) {
-                }
-
-                CardReader.prepare();
-
-                ID_Asker ask = new ID_Asker();
-                ask.setVisible(true);
-
-//                JFrame frame = new JFrame("JavaFX 2 in Swing");
-//                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//                
-//                JApplet applet = new Main();
-//                applet.init();
-//                
-//                frame.setContentPane(applet.getContentPane());
-//                
-//                frame.pack();
-//                frame.setLocationRelativeTo(null);
-//                frame.setVisible(true);
-//                
-//                applet.start();
-            }
-        });
+        ID_Asker ask = new ID_Asker();
+        ask.setVisible(true);
     }
-
-    @Override
-    public void init() {
-       /* fxContainer = new JFXPanel();
-        fxContainer.setPreferredSize(new Dimension(JFXPANEL_WIDTH_INT, JFXPANEL_HEIGHT_INT));
-        add(fxContainer, BorderLayout.CENTER);
-        // create JavaFX scene
-        Platform.runLater(new Runnable() {
-
-            @Override
-            public void run() {
-                createScene();
-            }
-        });*/
-    }
-
-  /*  private void createScene() {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        fxContainer.setScene(new Scene(root));
-    }*/
 
 }
